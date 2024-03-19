a37fb467ca5f5afa34614e139869e8951ac6acbb
alexkiriungi <mwasxander@gmail.com>
2024-03-19 17:25:36 +0300

    Homepage Routing

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    children: React.ReactNode;
+}
+
+const maarketingLayout = ({ children }: Props) => {
+    return (
+        <div>{children}</div>
+    );
+};
+
+export default maarketingLayout;
