8d913c21ca148b136601f0f0509b345fd809178d
alexkiriungi <mwasxander@gmail.com>
2024-03-20 11:53:09 +0300

    finished up layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,19 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+
 type Props = {
     children: React.ReactNode;
 }
 
 const maarketingLayout = ({ children }: Props) => {
     return (
-        <div>{children}</div>
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 flex flex-col items-center justify-center">
+                {children}
+            </main>
+            <Footer />
+        </div>
     );
 };
 
