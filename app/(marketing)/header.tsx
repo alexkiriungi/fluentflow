bd9fd3ae25267935acc608d7f80d40859303b988
alexkiriungi <mwasxander@gmail.com>
2024-03-23 17:07:59 +0300

    Hero section added

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
         <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
             <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                <Image src="/rabbit.png" height={40} width={40} alt="Rabbit" />
+                <Image src="/mascot.svg" height={40} width={40} alt="Logo" />
                 <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">FluentFlow</h1>
             </div>
             <ClerkLoading>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/./components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-green-500 ">Hello Fluentflow</p>
-      <Button>click me</Button>
+    <div className="max-w-[988px] mx-auto w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
     </div>
-  )
-}
+  );
+};
