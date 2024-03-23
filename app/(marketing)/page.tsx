1126eabe97480eb3205b636e9f8b68c13a2ed25e
alexkiriungi <mwasxander@gmail.com>
2024-03-23 17:55:10 +0300

    minor changes

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center gap-y-8">
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
-          Learn, practice, and master new languages with Lingo.
+        Revolutionize Your Language Learning Journey with Our Powerful Webapp
         </h1>
         <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
