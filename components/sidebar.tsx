6fca5ff87e669b8381d0cddd366275cabc6e9574
alexkiriungi <mwasxander@gmail.com>
2024-03-24 08:56:04 +0300

    Added Utilities

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,25 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebaritem";
 type Props = {
     className?: string;
 }
 
 export const Sidebar = ({ className }:Props) => {
   return (
-    <div className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+    <div className={cn("flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
     className
     )}>
-        Sidebar
+        <Link href="/learn">
+            <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                <Image src="/mascot.svg" height={40} width={40} alt="Logo" />
+                <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">FluentFlow</h1>
+            </div>
+        </Link>
+        <div className="flex flex-col gap-y-2 flex-1">
+            <SidebarItem label="learn" href="/learn" iconSrc="/learn.svg" />
+        </div>
     </div>
   );
 };
