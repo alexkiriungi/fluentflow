b7407ab68c5b1acce43ece582f7be9920c7e4d89
alexkiriungi <mwasxander@gmail.com>
2024-03-24 08:35:09 +0300

    added mobile sidebar

--- components/mobileheader.tsx
+++ components/mobileheader.tsx
@@ -1,7 +1,9 @@
+import { MobileSidebar } from "./mobilesidebar";
+
 export const MobileHeader = () => {
     return (
-        <nav className="lg:hidden px-6 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50">
-            Mobile Header
+        <nav className="lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50">
+            <MobileSidebar />
         </nav>
     );
 };
