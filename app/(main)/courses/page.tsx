5a4b58c62adb305aa6476f941e5d71726ebbaceb
alexkiriungi <mwasxander@gmail.com>
2024-03-29 22:08:26 +0300

    minor changes

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,11 +1,18 @@
-import { getCourses, getUserProgress} from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 
 import { List } from "./list";
 
 const CoursesPage = async () => {
-  const courses = await getCourses();
-  const userProgress = await getUserProgress();
+  const coursesData = getCourses();
+  const userProgressData = getUserProgress();
 
+  const [
+    courses,
+    userProgress,
+  ] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
